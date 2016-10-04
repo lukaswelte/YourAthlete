1b5124e83612e455b7b75f03fd985b87cb692118
Lukas Welte <info@lukaswelte.de>
2016-10-04 11:10:37 +0200

    changed base font to helvetica neue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ class Home extends PureComponent {
             title="Was der Athlet sagt:"
             photoURL="http://www.dsv.de/typo3temp/pics/Schwimmen_Vogel_Florian_2016_2b254d144f.jpg"
             name="Florian Vogel"
-            description="Olympionike in Rio über 400m Freistil"
+            description="Olympionike über 400m Freistil"
             quoteBackgroundURL="https://hd.unsplash.com/photo-1438029071396-1e831a7fa6d8">
             Durch YourAthlets erhalten wir Leistungssportler in Deutschland ganz neue Möglichkeiten.<br />
             Die Förderung erlaubt mir die volle Konzentration auf das Schwimmen zu legen und mir bleibt ausreichend Zeit mich durch mein Studium auf das Leben nach dem aktiven Schwimmen vorzubereiten
